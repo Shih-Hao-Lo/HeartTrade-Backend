9782ea727094ad6fef7ad5e3ee09c8d64e19c26c
Hao-Ping Lin <pinglin927@gmail.com>
2020-03-26 14:58:18 -0400

    Add entry

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,37 @@
-
 const constructorMethod = app => {
     app.get("/" , (req , res) => {
         res.sendStatus(200);
     });
+
+    app.get('/register', function(req, res) {
+        res.send('register', { });
+    });
+
+    app.post('/register', function(req, res) {
+        Account.register(new Account({username: req.body.username}), req.body.password, function(err, account) {
+            if (err) {
+                return res.send('register', {account: account});
+            }
+
+            passport.authenticate('local')(req, res, function() {
+                res.redirect(301, '/');
+            });
+        })
+    });
+
+    app.get('/login', function(req, res) {
+        res.send('login', {user: req.user});
+    });
+
+    app.post('/login', passport.authenticate('local'), function(req, res) {
+        res.redirect('/');
+    });
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.direct('/');
+    });
+
 }
 
 module.exports = constructorMethod;
