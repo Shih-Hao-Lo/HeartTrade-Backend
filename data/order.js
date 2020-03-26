c172c9c51bef3b69d57b6c55b425d07cba6f2739
Shih-Hao-Lo <slo@stevens.edu>
2020-03-26 15:25:24 -0400

    add database function

--- data/order.js
+++ data/order.js
@@ -2,6 +2,48 @@ const mongoCollections = require("./mongoCollections");
 const orders = mongoCollections.orders;
 
 /*
+    in: 
+        id: ID!
+    ret:
+        order = {
+                _id: ID!
+                user_id: String!
+                prod: String!
+                amt: Integer!
+                wish: String!
+                wish_amt: Integer!
+            }
+
+*/
+async function get(id) {
+    const ordersCollections = await orders();
+    const target = await ordersCollections.findOne({ _id: id });
+
+    return target;
+}
+
+/*
+    in: 
+        uid: String!
+    ret:
+        order[] = [{
+                _id: ID!
+                user_id: String!
+                prod: String!
+                amt: Integer!
+                wish: String!
+                wish_amt: Integer!
+            }]
+
+*/
+async function getbyuser(uid) {
+    const ordersCollections = await orders();
+    const targets = await ordersCollections.find({ user_id: uid });
+
+    return targets;
+}
+
+/*
     in:
         user_id: String!
         prod: String!
@@ -9,7 +51,7 @@ const orders = mongoCollections.orders;
         wish: String!
         wish_amt: Integer!
     ret:
-        obj = {
+        order = {
             _id: ID!
             user_id: String!
             prod: String!
@@ -18,8 +60,20 @@ const orders = mongoCollections.orders;
             wish_amt: Integer!
         }
 */
-function addorders(user_id , prod , amt , wish , wish_amt) {
+async function addorders(user_id , prod , amt , wish , wish_amt) {
+    const ordersCollections = await orders();
 
+    let neworder = {
+        user_id: user_id.toString(),
+        prod: prod,
+        amt: amt,
+        wish: wish,
+        wish_amt: wish_amt
+    }
+
+    const inserted = await ordersCollections.insertOne(neworder);
+
+    return await get(inserted.insertedId);
 }
 
 
@@ -31,7 +85,7 @@ function addorders(user_id , prod , amt , wish , wish_amt) {
         wish: String!
         wish_amt: Integer!
     ret:
-        obj = {
+        order = {
             _id: ID!
             user_id: String!
             prod: String!
@@ -41,5 +95,33 @@ function addorders(user_id , prod , amt , wish , wish_amt) {
         }
 */
 function updateorders(post_id , prod , amt , wish , wish_amt){
+    let target = await get(post_id);
+    const ordersCollections = await orders();
+    if(prod == undefined) prod = target.prod;
+    if(amt == undefined) amt = target.amt;
+    if(wish == undefined) wish = target.wish;
+    if(wish_amt == undefined) wish_amt = target.wish_amt;
 
+
+    let updatedorder = {
+        $set: {
+            _id: target._id,
+            user_id: target.user_id,
+            prod: prod,
+            amt: amt,
+            wish: wish,
+            wish_amt: wish_amt
+        }
+    }
+
+    const updated = await ordersCollections.updateOne({ _id: post_id } , updatedorder);
+
+    return await get(post_id);
 }
+
+module.exports = {
+    get,
+    getbyuser,
+    addorders,
+    updateorders
+};
